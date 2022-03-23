ac7f2a2958684408255151c04749877f5fe1e3ea
Anurag30112003 <anuragsharma011011@gmail.com>
2022-03-23 21:43:56 +0530

    success page

--- src/views/Success.vue
+++ src/views/Success.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="text-center font-Poppins">Successfully Loggedin</div>
+    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 font-Poppins text-5xl">Successfully Loggedin</div>
 </template>
 <script>
 export default{
